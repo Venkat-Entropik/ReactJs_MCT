365c9089d5c3d5f07ff2899e4c61dad41aa33172
venky123895 <devathotivenkataramana@gmail.com>
2023-08-08 09:45:26 +0530

    chenged

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,23 +1,8 @@
-// const userData={
-//     userDetails:[]
-// }
-// export const userDataReducer=(state=userData,{type,data})=>{
-//     switch (type) {
-//         case "USERDATA":
-            
-//             return{...state,userDetails:data}
-    
-//         default:
-//             return state;
-//     }
-// }
 
 const allBlogData=[
 
 ]
    
-
-
 export const blogDataReducer=(state=allBlogData,action)=>{
     switch (action.type) {
         case "BLOGDATA":
@@ -25,9 +10,6 @@ export const blogDataReducer=(state=allBlogData,action)=>{
             return[...state,action.data]
         case "EDIT":
             return [...state.map((item)=>{
-                // console.log("item",item);
-                // console.log('id',item.id);
-                // console.log('newId',item.id);
                 return item.id === action.data.id ? {...item,title:action.data.title,description:action.data.description} : item
             })]
         default:
